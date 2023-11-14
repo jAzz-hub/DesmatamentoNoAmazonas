8d5d23ffb80476289b4ddb6e39cdd5b8eff5c263
jAzz-hub <joaogu.2001@gmail.com>
2023-11-13 22:40:56 -0300

    Data Cleaning | Adicionando mapa atual dos municípios | Construindo Listas de Adjascência entre municípios

---
+++ Notebooks/FronteirasMunicípios.py
@@ -0,0 +1,185 @@
+#Urucurituba x
+['Boa Vista do Ramos', 'Barreirinha', 'Itapiranga', 'Itacoatiara', 'Parintins', 'São Sebastião do Uatumã', 'Urucarã', 'Silves', 'Nhamundá']
+
+#Urucara x
+['Nhamundá', 'Urucurituba', 'Itapiranga', 'São Sebastião do Uatumã', 'Presidente Figueiredo']
+
+#Uarini x
+['Manaã','Fonte Boa','Juruá', 'Alvarães','Tefé']
+
+#Tonantins x
+['Japurá', 'Santo Antônio do Içá', 'Fonte Boa', 'Jutaí']
+
+#Tefe x
+['Tapauá', 'Manaã', 'Carauari', 'Juruá', 'Alvarães', 'Uarini']
+
+#Tapauá x
+['Itamarati', 'Pauini', 'Lábrea', 'Canutama', 'Carauari', 'Tefé', 'Coari', 'Anori', 'Beruri', 'Manicoré', 'Humaitá']
+
+#Tabatinga
+['Leticia', 'Santa Rosa do Purus', 'São Paulo de Olivença', 'Tapauá']
+
+#Silves
+['Atalaia do Norte', 'Barreirinha', 'Itacoatiara', 'Manacapuru', 'Tonantins']
+
+#Sao Sebastiao do Uatuma
+['Novo Airão', 'São Gabriel da Cachoeira', 'Silves']
+
+#Sao Paulo de Olivenca
+['Leticia', 'Tabatinga', 'Santa Rosa do Purus']
+
+#Sao Gabriel da Cachoeira
+['Novo Airão', 'Sao Sebastiao do Uatuma', 'Santa Isabel do Rio Negro']
+
+#Santo Antonio do Ica
+['Eirunepé', 'Tabatinga', 'Santa Isabel do Rio Negro']
+
+#Santa Isabel do Rio Negro
+['Eirunepé', 'Leticia', 'Novo Airão', 'Sao Gabriel da Cachoeira', 'Santo Antonio do Ica']
+
+#Rio Preto da Eva
+['Itacoatiara', 'Manacapuru', 'Maués', 'Parintins']
+
+#Presidente Figueiredo
+['Manacapuru', 'Maués', 'Pauini']
+
+#Pauini x
+['Boca do Acre', 'Envira', 'Itamarati', 'Eirunepé','Tapauá', 'Lábrea']
+
+#Parintins
+['Itacoatiara', 'Manacapuru', 'Maués', 'Rio Preto da Eva', 'Pauini']
+
+#Novo Aripuana
+['Manicoré', 'Apuí', 'Borba']
+
+#Novo Airão
+['Japurá', 'Maués', 'Silves', 'São Sebastião do Uatumã']
+
+#Nova Olinda do Norte
+['Japurá', 'Maués', 'Pauini']
+
+#Nhamunda
+['Coari', 'Iranduba', 'Manacapuru', 'Maués']
+
+#Maués
+['Atalaia do Norte', 'Barreirinha', 'Itacoatiara', 'Manacapuru', 'Novo Aripuana', 'Parintins', 'Rio Preto da Eva', 'São Sebastião do Uatumã']
+
+#Maraã
+['Atalaia do Norte', 'Japurá', 'Maués', 'Tonantins']
+
+#Manicoré x
+['Humaitá', 'Tapauá', 'Beruri', 'Borba','Novo Aripuana']
+
+#Manaus
+['Careiro Castanho', 'Careiro da Várzea', 'Iranduba', 'Manacapuru', 'Novo Airão', 'Presidente Figueiredo', 'Rio Preto da Eva']
+
+#Manaquiri
+['Manacapuru', 'Maués', 'Parintins', 'Presidente Figueiredo']
+
+#Manacapuru
+['Atalaia do Norte', 'Barreirinha', 'Itacoatiara', 'Maués', 'Nova Olinda do Norte', 'Nhamunda', 'Parintins', 'Presidente Figueiredo', 'Rio Preto da Eva']
+
+#Lábrea x
+['Boca do Acre', 'Pauini', 'Itamarati', 'Tapauá', 'Canutama']
+
+#Jutaí x 
+['Itamarati', 'Eirunepé', 'Benjamin Constant', 'São Paulo de Olivença','Amatur','Santo Antônio do Içá', 'Tonantins', 'Fonte Boa', 'Juruá', 'Carauari']
+
+#Juruá
+['Atalaia do Norte', 'Itamarati', 'Japurá', 'Lábrea', 'Santa Isabel do Rio Negro']
+
+#Japurá
+['Atalaia do Norte', 'Benjamim Constant', 'Coari', 'Humaitá', 'Itamarati', 'Manacapuru', 'Manicoré', 'Maués', 'Maraã', 'Novo Aripuana', 'Parintins', 'Tapauá']
+
+#Itapiranga
+['Atalaia do Norte', 'Benjamin Constant', 'Itamarati', 'Japurá']
+
+#Itamarati
+['Atalaia do Norte', 'Eirunepé', 'Juruá', 'Itapiranga', 'Japurá']
+
+#Itacoatiara
+['Barreirinha', 'Manacapuru', 'Maués', 'Rio Preto da Eva', 'São Sebastião do Uatumã']
+
+#Iranduba
+['Careiro Castanho', 'Careiro da Várzea', 'Manacapuru', 'Manaquiri', 'Maués', 'Nhamunda']
+
+#Ipixuna
+['Eirunepé', 'Japurá', 'Lábrea', 'Tapauá']
+
+#Humaita x
+['Canutama', 'Tapauá', 'Manicoré']
+
+#Guajara
+['Benjamin Constant', 'Eirunepé', 'Humaitá']
+
+#Fonte Boa
+['Eirunepé', 'Humaitá', 'Itamarati', 'Juruá']
+
+#Envira x
+['Pauini', 'Itamarati', 'Eirunepé']
+
+#Eirunepe x
+['Benjamin Constant', 'Ipixuna', 'Envira', 'Itamarati', 'Jutaí','Pauini']
+
+#Codajas
+['Coari', 'Manacapuru', 'Maués', 'Santa Isabel do Rio Negro']
+
+#Coari
+['Codajas', 'Humaitá', 'Manacapuru', 'Maués', 'Novo Aripuana']
+
+#Careiro da Varzea
+['Iranduba', 'Manacapuru', 'Maués']
+
+#Careiro
+['Iranduba', 'Manacapuru', 'Maués']
+
+#Carauari
+['Canutama', 'Humaitá', 'Iranduba']
+
+#Canutama x 
+['Lábrea', 'Tapauá', 'Humaitá']
+
+#Caapiranga
+['Manacapuru', 'Maués', 'Novo Aripuana']
+
+#Borba
+['Santa Isabel do Rio Negro', 'Japurá', 'Maués', 'Novo Aripuana', 'Parintins']
+
+#Boca do Acre x
+['Pauini', 'Lábrea']
+
+#Boa Vista do Ramos
+['Boca do Acre', 'Coari', 'Japurá']
+
+#Beruri
+['Boa Vista do Ramos', 'Coari', 'Japurá']
+
+#Benjamin Constant
+['Boca do Acre', 'Guajara', 'Humaitá', 'Itamarati', 'Japurá']
+
+#Barreirinha
+['Manacapuru', 'Maués', 'Pauini']
+
+#Barcelos
+['Santa Isabel do Rio Negro', 'Japurá', 'Maués', 'Nova Olinda do Norte', 'Parintins']
+
+#Autazes
+['Iranduba', 'Manacapuru', 'Parintins', 'São Sebastião do Uatumã']
+
+#Atalaia do Norte x 
+['Benjamim Constant', 'Guajará','Ipixuna']
+
+#Apui x
+['Novo Aripuanã', 'Borba', 'Mauês']
+
+#Anori
+['Atalaia do Norte', 'Japurá', 'Maraã']
+
+#Anama
+['Atalaia do Norte', 'Japurá']
+
+#Amatura
+['Atalaia do Norte', 'Japurá']
+
+#Alvarães
+['Santa Isabel do Rio Negro', 'Japurá', 'Maraã', 'Parintins']
--- README.md
+++ README.md
@@ -24,3 +24,61 @@ O projeto seguirá o ciclo de vida [OSEMN(Obtain Scrub Explore Model iNterpret)]
 ‌
 
 ‌
+<!-- IDEIAS 
+ 
+ <!-- 1. Existe uma correlação entre conectividade e Desmatamento?
+
+        1. Atribui-se peso aos cliques buscando os números de desmatamento total em cada clique
+
+        2. Atribui-se 
+ -->
+ 
+ <!-- 2. Correlação Local?
+
+        Considere a correlação local, onde você examina a correlação entre o desmatamento de um município e o desmatamento de seus vizinhos diretamente conectados. Isso pode revelar padrões locais na relação entre conectividade e desmatamento.
+ -->
+
+
+ <!-- 3. Identificação de Cliques?
+
+        Observando cliques verifique se eles possuem características semelhantes sobre desmatamento ou sobre urbanização, ou sobre demais métricas.
+
+
+ -->
+
+
+  <!-- 4. Análise de Fluxo nos algoritmos?
+        Explore algoritmos de fluxo em grafos para entender como a conectividade impacta a disseminação do desmatamento através da rede viária. Considere algoritmos de caminho mínimo ponderado para modelar o impacto ambiental da conectividade.
+ -->
+
+<!-- 5. Centralidade dos vértices
+        Calcule métricas de centralidade dos vértices, como a centralidade de grau, centralidade de proximidade ou centralidade de intermediação, para identificar os municípios mais centrais em termos de conectividade. Analise se esses municípios centrais têm padrões distintos de desmatamento.
+
+ -->
+
+
+<!-- PERSISTENTES 
+
+    <!-- Análise de Centralidade 
+        Centralidade de Proximidade (Closeness Centrality):
+        Centralidade Harmonic (Harmonic Centrality):
+        Centralidade de PageRank:
+        Centralidade de Katz:
+
+
+    -->
+
+
+
+    <!-- Identificação de Cliques -->
+
+-->
+
+
+<!-- ESTRATÉGIAS INTERESSANTES INDEPENDENTE DE TUDO 
+    <!-- Normalização de dados de desmatamento:
+        Criação de uma escala de desmatamento em uma escala comum de 0 a 1
+        Responder a pergunta: Por quê a normalização dos dados é interessante?
+    -->
+
+    -->
