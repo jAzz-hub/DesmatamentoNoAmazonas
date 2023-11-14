e87c7414356660be3eba3932550aa87527162fd4
jAzz-hub <joaogu.2001@gmail.com>
2023-11-14 18:07:00 -0300

    Data Cleaning | Criando Coluna com municípios adjascêntes | Consertando o README.md

---
+++ Notebooks/FronteirasMunicípiosDict.py
@@ -0,0 +1,187 @@
+MunicipalAdjascencyDict = {
+'Urucurituba' : 
+['Boa Vista do Ramos', 'Barreirinha', 'Itapiranga', 'Itacoatiara', 'Parintins', 'Urucara', 'Silves', 'Nhamunda'],
+
+'Urucara' : 
+['Nhamunda', 'Urucurituba', 'Itapiranga', 'Sao Sebastiao do Uatuma', 'Presidente Figueiredo'],
+
+'Uarini' : 
+['Manaa','Fonte Boa','Jurua', 'Alvaraes','Tefe'],
+
+'Tonantins' : 
+['Japura', 'Santo Antonio do Iça', 'Fonte Boa', 'Jutai'],
+
+'Tefe' : 
+['Tapaua', 'Manaa', 'Carauari', 'Jurua', 'Alvaraes', 'Uarini'],
+
+'Tapaua' : 
+['Itamarati', 'Pauini', 'Labrea', 'Canutama', 'Carauari', 'Tefe', 'Coari', 'Anori', 'Beruri', 'Manicore', 'Humaita'],
+
+'Tabatinga' : 
+['Benjamin Constant', 'Santo Antonio do Iça', 'Sao Paulo de Olivença'],
+
+'Silves' : 
+[ 'Urucurituba', 'Itacoatiara', 'Itapiranga'],
+
+'Sao Sebastiao do Uatuma' : 
+['Urucura', 'Presidente Figueiredo', 'Itapiranga'],
+
+'Sao Paulo de Olivenca' : 
+['Benjamin Constant', 'Jutai', 'Tabatinga', 'Santo Antonio do Iça', 'Amatur'],
+
+'Sao Gabriel de Cachoeira' : 
+['Japura', 'Santa Isabel do Rio Negro'],
+
+'Santo Antonio do Ica' : 
+['Sao Paulo de Olivença', 'Tabatinga', 'Amatur' , 'Tonantins', 'Jutai', 'Japura'],
+
+'Santa Isabel do Rio Negro' : 
+['Sao Gabriel da Cachoeira', 'Japura', 'Manaa', 'Barcelos'],
+
+'Rio Preto da Eva' : 
+['Itacoatiara', 'Manaus', 'Itapiranga', 'Presidente Figueiredo'],
+
+'Presidente Figueiredo' : 
+['Rio Preto de Eva', 'Manaus', 'Novo Airao', 'Itapiranga', 'Sao Sebastiao do Uatuma', 'Urucua'],
+
+'Pauini' : #15
+['Boca do Acre', 'Envira', 'Itamarati', 'Eirunepe','Tapaua', 'Labrea'],
+
+'Parintins' : #16
+['Nhamunda', 'Urucurituba', 'Barreirinha'],
+
+'Novo Aripuana' : 
+['Manicore', 'Apui', 'Borba'],
+
+'Novo Airao' :  #18
+['Barcelos', 'Codajas', 'Caapiranga', 'Manacapuru', 'Manaus', 'Presidente Figueiredo'],
+
+'Nova Olinda do Norte' : 
+['Borba', 'Maues', 'Autazes', 'Itacoatiara'],
+
+'Nhamunda' :  
+['Urucura', 'Urucurituba', 'Parintins', 'Maues'],
+
+'Maues' : #21
+['Apui', 'Borba', 'Itacoatiara', 'Boa Vista do Ramos', 'Barreirinha', 'Nova Olinda do Norte'],
+
+'Manaa' : 
+['Fonte boa', 'Japura', 'Santa Izabel do Rio Negro', 'Barcelos', 'Codajas', 'Coari', 'Tefe', 'Uarini'],
+
+'Manicore' : 
+['Humaita', 'Tapaua', 'Beruri', 'Borba','Novo Aripuana'],
+
+'Manaus' : 
+['Manacapuru','Itacoatiara','Careiro', 'Careiro da Varzea', 'Iranduba', 'Manacapuru', 'Novo Airao', 'Presidente Figueiredo', 'Rio Preto da Eva'],
+
+'Manaquiri' : 
+['Manacapuru', 'Borba', 'Beruri', 'Iranduba', 'Manaus', 'Careiro'],
+
+'Manacapuru' : 
+['Novo Airao', 'Caapiranga', 'Anama', 'Anori', 'Beruri', 'Manaquiri', 'Iranduba', 'Manaus'],
+
+'Labrea' : 
+['Boca do Acre', 'Pauini', 'Itamarati', 'Tapaua', 'Canutama'],
+
+'Jutai' :  
+['Itamarati', 'Eirunepe', 'Benjamin Constant', 'Sao Paulo de Olivença','Amatur','Santo Antonio do Iça', 'Tonantins', 'Fonte Boa', 'Jurua', 'Carauari'],
+
+'Jurua' :  
+['Carauari', 'Jutai', 'Fonte Boa', 'Uarini', 'Alvaraes', 'Tefe'],
+
+'Japura' : 
+['Santo Antonio de Iça',' Tonantins','Fonte Boa', 'Manaa','Santa Isabel do Rio Negro',' Sao Gabriel de Cachoeira'],
+
+'Itapiranga' :  
+['Presidente Figueiredo','Sao Sebastiao do Uatuma','Urucura','Urucurituba', 'Silves', 'Rio Preto de Eva', 'Itacoatiara'],
+
+'Itamarati' : 
+['Envira', 'Eirunepe', 'Jutai', 'Carauari', 'Tapua','Labrea','Pauini'],
+
+'Itacoatiara' : 
+['Autazes', 'Careiro da Varzea', 'Maues', 'Rio Preto da Eva', 'Nova Olinda do Norte', 'Manaus', 'Silves', 'Itapiranga', 'Urucurituba', 'Boa Vista do Ramos'],
+
+'Iranduba' :  
+['Novo Airao','Careiro', 'Careiro da Varzea', 'Manacapuru', 'Manaquiri', 'Manaus'],
+
+'Ipixuna' : 
+['Eirunepe', 'Benjamin Constant', 'Atalaia do Norte', 'Guajara'],
+
+'Humaita' : 
+['Canutama', 'Tapaua', 'Manicore'],
+
+'Guajara' : 
+['Atalaia do Norte', 'Ipixuna'],
+
+'Fonte Boa' : 
+['Jutai', 'Jurua', 'Tonantins', 'Japura', 'Manaa', 'Uarini'],
+
+'Envira' : 
+['Pauini', 'Itamarati', 'Eirunepe'],
+
+'Eirunepe' : 
+['Benjamin Constant', 'Ipixuna', 'Envira', 'Itamarati', 'Jutai','Pauini'],
+
+'Codajas' : 
+['Anama','Anori','Coari','Manaa','Barcelos','Novo Airao','Caapiranga'],
+
+'Coari' : 
+['Manaa', 'Codajas', 'Tefe', 'Tapua', 'Anori','Barcelos'],
+
+'Careiro da Varzea' : 
+['Iranduba', 'Careiro', 'Manaus', 'Itacoatiara', 'Autazes'],
+
+'Careiro' : 
+['Iranduba', 'Manacapuru', 'Manaquiri', 'Borba', 'Autazes', 'Careiro da Varzea', 'Manaus'],
+
+'Carauari' : 
+['Tefe', 'Alvaraes', 'Jurua', 'Jutai', 'Itamarati', 'Tapua'],
+
+'Canutama' :  
+['Labrea', 'Tapaua', 'Humaita'],
+
+'Caapiranga' : 
+['Anama', 'Manacapuru', 'Novo Airao', 'Codajas'],
+
+'Borba' : 
+['Maues', 'Nova Olinda do Norte', 'Autazes', 'Careiro', 'Manaquiri', 'Beruri', 'Manicore', 'Novo Aripuana', 'Apui'],
+
+'Boca do Acre' : 
+['Pauini', 'Labrea'],
+
+'Boa Vista do Ramos' : 
+['Maues','Barreirinha', 'Urucurituba', 'Itacoatiara'],
+
+'Beruri' : 
+['Manicore', 'Tapua', 'Anori', 'Anama', 'Manacapuru', 'Manaquiri'],
+
+'Benjamin Constant' : 
+['Atalaia do Norte', 'Ipixuna', 'Eirunepe', 'Jutai', 'Sao Paulo de Olivença', 'Tabatinga'],
+
+'Barreirinha' : 
+['Maues', 'Boa Vista do Ramos', 'Urucurituba', 'Parintins'],
+
+'Barcelos' : 
+['Santa Isabel do Rio Negro', 'Novo Airao', 'Manaa', 'Codajas', 'Coari'],
+
+'Autazes' : 
+['Manaus', 'Careiro', 'Careiro da Varzea', 'Borba', 'Itacoatiara', 'Nova Olinda do Norte'],
+
+'Atalaia do Norte' :  
+['Benjamim Constant', 'Guajara','Ipixuna'],
+
+'Apui' : 
+['Novo Aripuana', 'Borba', 'Maues'],
+
+'Anori' : 
+['Beruri', 'Tapua', 'Coari', 'Codajas', 'Anama'],
+
+'Anama' :  
+['Manacapuru', 'Beruri',' Anori','Codajas','Caapiranga'],
+
+'Amatur' : 
+['Sao Paulo de OLivença', 'Jutai', 'Santo Antonio do Iça'],
+
+'Alvaraes' : 
+['Tefe', 'Carauari', 'Jurua', 'Uarini'],
+}
